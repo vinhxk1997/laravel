14673341b283096e1aa87172190283cc43f01e8d
vinhxk1997 <nguyenvanvinhxk1997@gmail.com>
2018-12-20 16:46:46 +0700

    new project

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\CommentFormRequest;
+use App\Comment;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function newComment(CommentFormRequest $request)
+    {
+        $comment = new Comment(array(
+            'post_id' => $request->get('post_id'),
+            'content' => $request->get('content'),
+            'post_type' => $request->get('post_type')
+        ));
+        $comment->save();
+        return redirect()->back()->with('status', 'Your comment has been created!');
+    }
+}
